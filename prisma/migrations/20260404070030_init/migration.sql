52619bd57e794da3756d2eb11dc90f56ec713da3
Deepyaman Mondal <deepyamanmondal7@gmail.com>
2026-04-04 12:31:54 +0530

    Initialize database migration files

---
+++ prisma/migrations/20260404070030_init/migration.sql
@@ -0,0 +1,52 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('VIEWER', 'ANALYST', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "RecordType" AS ENUM ('INCOME', 'EXPENSE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'VIEWER',
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Record" (
+    "id" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "type" "RecordType" NOT NULL,
+    "category" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "description" TEXT,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Record_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Record_userId_idx" ON "Record"("userId");
+
+-- CreateIndex
+CREATE INDEX "Record_date_idx" ON "Record"("date");
+
+-- CreateIndex
+CREATE INDEX "Record_category_idx" ON "Record"("category");
+
+-- CreateIndex
+CREATE INDEX "Record_type_idx" ON "Record"("type");
+
+-- AddForeignKey
+ALTER TABLE "Record" ADD CONSTRAINT "Record_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
